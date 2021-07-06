25e0f1fb5027e5d8c35f986d039212ec4830f03f
Alfredo Bonilla <brolag@live.com>
2021-07-05 20:36:08 -0600

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:tailwind/recommended",
+    "standard",
+    "prettier",
+    "next",
+    "next/core-web-vitals",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "func-names": "off",
+    "no-process-exit": "off",
+    "object-shorthand": "off",
+    "class-methods-use-this": "off",
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from "next/head";
-import { Component } from "react";
+import React, { Component } from "react";
 import { attributes, react as HomeContent } from "../content/home.md";
+import Footer from "@components/Footer";
 
 export default class Home extends Component {
   render() {
     let { title, cats } = attributes;
     return (
-      <>
+      <main className="flex flex-col h-screen justify-between bg-gradient-to-r from-blue-900 to-black">
         <Head>
           <script
             dangerouslySetInnerHTML={{
@@ -14,19 +15,24 @@ export default class Home extends Component {
             }}
           />
         </Head>
-        <article>
-          <h1>{title}</h1>
-          <HomeContent />
-          <ul>
-            {cats.map((cat, k) => (
-              <li key={k}>
-                <h2>{cat.name}</h2>
-                <p>{cat.description}</p>
-              </li>
-            ))}
-          </ul>
-        </article>
-      </>
+        <div className="flex-col justify-start">
+          <section className="text-white p-20">
+            <h1 className="text-4xl">{title}</h1>
+          </section>
+          <section className="text-white p-20">
+            <HomeContent />
+            <ul>
+              {cats.map((cat, k) => (
+                <li key={k}>
+                  <h2>{cat.name}</h2>
+                  <p>{cat.description}</p>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
+        <Footer />
+      </main>
     );
   }
 }
